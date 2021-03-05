3b297e2efc1b3516c3bd1f5dc96ee8924c0618c0
Pooya Shirazi <p.shirazi.a@gmail.com>
2021-03-05 13:17:43 +0330

    Add Expense Model

---
+++ bestoonApp/home/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Expense
+
+# Register your models here.
+admin.site.register(model_or_iterable=Expense)
---
+++ bestoonApp/home/models.py
@@ -0,0 +1,15 @@
+from django.contrib.admin.options import TabularInline
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}-{}-{} | {} T".format(self.date.year, self.date.month, self.date.day, self.amount)
